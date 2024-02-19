2fe0c451394a5681bb3757239e86ef616fdbd1f5
Mathieu <mathieuducharme86@gmail.com>
2024-02-18 20:20:16 -0500

    final version

--- date_of_easter.py
+++ date_of_easter.py
@@ -1,3 +1,9 @@
+# Q1: La fonction doit calculer la date de Pauqes d'une année donnée
+# Q2: La fonction a pour paramètre 'year' un int
+# Q3: La fonction retourne un string du format JJ-MM-AAAA
+# Q4: Le nom de la fonction est 'find_date_of_easter'
+# Q5: find_date_of_easter(2024) = '31-03-2024'
+
 """date_of_easter.py : Caclul les dates de Paques
 
 Ce fichier permet de calculer la date du dimanche de Paques d'une année donnée en utilisant
@@ -10,36 +16,36 @@ consultez 'https://fr.wikipedia.org/wiki/Calcul_de_la_date_de_P%C3%A2ques'.
 @Matricule: 20297456
 """
 
-import math
-from datetime import datetime
-from dateutil.easter import *
+def zfill(characters, total_length, leading_char=0):
+    """Retourne un string d'un longueur 'total_length' avec le caractère
+    'leading_char' qui remplit l'espace vide à gauche du string initial.
+    """
+    characters = str(characters)
+    leading_char_length = total_length - len(characters) # Nombre de caractère à ajouter à gauche
+    result = ''
 
-class MyString(str):
-    def zfill(self, total_length, leading_char=0):
-        leading_char_length = total_length - len(self)
-        result = ''
+    for i in range(leading_char_length):
+        result += str(leading_char)
 
-        for i in range(leading_char_length):
-            result += str(leading_char)
-
-        return result + self
+    return result + characters
     
-def mystr(number):
-    return MyString(str(number))
 
 def format_date(day, month, year):
-    return mystr(day).zfill(2) + '-' + mystr(month).zfill(2) + '-' + str(year)
+    """Prend en paramètre 3 int - soit le jour, le mois et l'année
+    puis retounre un string de la date sous la forme 'JJ-MM-AAAA'.
+    """
+    return zfill(day, 2) + '-' + zfill(month, 2) + '-' + str(year)
 
 def find_date_of_easter(year):
     """Trouve la date de Paques qui correspond au dimanche qui suit la première pleine
     lune du printemps en utilisant l'algorithme définit par le Concule de Nicée en 325.
 
     Args:
-        year (int): l'année que l'on souhaite connaitre la date de Paques
+        year (int): l'année dans l'intervalle [1583, 4999] que l'on souhaite connaitre la date de Paques
     Returns:
         ? (str): date de Paques sous la forme JJ-MM-YYYY ('%d-%m-%Y')
     """
-
+    
     # Golden year
     g = year % 19 
 
@@ -65,22 +71,19 @@ def find_date_of_easter(year):
     
     if r <= 31:
         return format_date(r, 3, year)
-        return f'{int(r):02d}-03-{year}'
     
     return format_date(r-31, 4, year)
-    return f'{int(r)-31:02d}-04-{year}'
 
 def test_find_date_of_easter():
-    assert find_date_of_easter(2024) == '31-03-2024'
+    assert find_date_of_easter(1583) == '10-04-1583'
+    assert find_date_of_easter(1655) == '28-03-1655'
     assert find_date_of_easter(2000) == '23-04-2000'
-    assert find_date_of_easter(2040) == '01-04-2040'
     assert find_date_of_easter(2007) == '08-04-2007'
+    assert find_date_of_easter(2024) == '31-03-2024'
+    assert find_date_of_easter(2040) == '01-04-2040'
+    assert find_date_of_easter(2100) == '28-03-2100'
+    assert find_date_of_easter(3100) == '22-04-3100'
+    assert find_date_of_easter(4999) == '07-04-4999'
 
-def test_super():
-    for date in range(1583, 5000):
-        assert find_date_of_easter(date) == easter(date).strftime('%d-%m-%Y'), \
-        f"{find_date_of_easter(date)} does not match {easter(date).strftime('%d-%m-%Y')}"
-
-test_find_date_of_easter()
-
-test_super()
+if __name__ == '__main__':
+    test_find_date_of_easter()
